f143bde3a977be37ec95c2f00a45aaa453b995ed
Thonny Noda <noda.thonny@gmail.com>
2022-01-26 17:11:45 -0300

    Atualizado em 26-01-2022

--- frontend/src/layout/Cadastros/index.js
+++ frontend/src/layout/Cadastros/index.js
@@ -1,25 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../../components/Card";
 
 import TitlePage from "../../components/TitlePage";
 import Dashbord from "../Dashbord";
-import { ContainerCadastros } from "./styles";
-
+import { BoxCadastros, ContainerCadastros, IconCadastro } from "./styles";
+import IconVagas from "../../assets/vagas.png";
 
 function Cadastros() {
+  const [open, setOpen] = useState("")
+
   return (
     <Dashbord>
       <TitlePage title="Cadastros" />
       <ContainerCadastros>
         <Card title="Vagas">
-          vaga1
+          <BoxCadastros>
+            <IconCadastro img={IconVagas} />
+            <h2>Vagas</h2>
+          </BoxCadastros>
         </Card>
-        <Card title="Vagas">
-          vaga1
-        </Card>
-        <Card title="Vagas">
-          vaga1
-        </Card>
+        <Card title="Vagas">vaga1</Card>
+        <Card title="Vagas">vaga1</Card>
       </ContainerCadastros>
     </Dashbord>
   );
--- frontend/src/layout/Cadastros/styles.js
+++ frontend/src/layout/Cadastros/styles.js
@@ -1,12 +1,74 @@
 import { styled } from "@mui/system";
 
-export const ContainerCadastros= styled('div')`
-  height:100%;
+export const ContainerCadastros = styled("div")`
+  height: 100%;
+  top: 80px;
   position: relative;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  @media screen (max-780px){
-
+  @media (max-width: 960px) {
+    flex-direction: column;
   }
-`; 
+`;
+
+export const BoxCadastros = styled("div")`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  //justify-content: center;
+  align-items: center;
+  &:hover h2 {
+    cursor: pointer;
+    color: green;
+  };
+  &:hover {
+    cursor: pointer;
+   
+  }
+`;
+
+export const IconCadastro = styled("div")`
+  width: 150px;
+  height: 150px;
+  background-image: url(${(props) => props.img});
+  background-size: 100% 100%;
+`;
+
+
+
+//**Modal */
+
+export const ModalDetalhe = styled("div")`
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.45);
+  z-index: 99;
+`;
+
+export const ContainerDetalhe = styled("div")`
+  max-width: 480px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 999;
+`;
+
+export const BoxDetalhe = styled("div")`
+  width: 100%;
+  padding: 10px;
+  display: flex;
+`;
+
+export const TitleDetalhe = styled("h2")`
+  font-weight: lighter;
+`;
+export const TextDetalhe = styled("p")`
+  &:nth-of-type(1) {
+    margin-right: 10px;
+  }
+`;
